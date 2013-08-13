6d4ac3a608ab61554f9ffddb9af65744b4070498
Alexandru Chelariu <alex1991.9@gmail.com>
2013-08-14 01:04:17 +0300

    Added option value xem

--- src/xwiki/command/InstallCommand.php
+++ src/xwiki/command/InstallCommand.php
@@ -5,6 +5,7 @@ namespace XWiki\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class InstallCommand extends Command
@@ -13,12 +14,17 @@ class InstallCommand extends Command
     {
         $this->setName('install')
             ->setDescription('Install an xwiki')
-            ->addArgument('version', InputArgument::REQUIRED, 'Version of xwiki');
+            ->addArgument('version', InputArgument::REQUIRED, 'Version of xwiki')
+            ->addOption("xem", null, InputOption::VALUE_NONE, "If you want xem version");
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $version = $input->getArgument('version');
         $output->writeln("<info>{$version}</info>");
+
+        if ($input->getOption('xem')) {
+            $output->writeln("Is xem version");
+        }
     }
 }
