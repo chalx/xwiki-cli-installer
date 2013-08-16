e7265201ed0dc7580e0191b960f1eee44c69f490
Alexandru Chelariu <alex1991.9@gmail.com>
2013-08-16 13:38:48 +0300

    Get download url

--- src/xwiki/config/XWikiVersion.php
+++ src/xwiki/config/XWikiVersion.php
@@ -2,6 +2,38 @@
 
 namespace XWiki\Config;
 
+use XWiki\Exceptions\FileNotFoundException;
+use XWiki\Exceptions\VersionNotFoundException;
+
 class XWikiVersion {
-    
+    private $versions;
+
+    /**
+     * @throws FileNotFoundException
+     */
+    public function __construct() {
+        $versionFile = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . "resources" . DIRECTORY_SEPARATOR . "versions.json";
+        if (file_exists($versionFile)) {
+            $this->versions = json_decode(file_get_contents($versionFile), true);
+        } else {
+            throw new FileNotFoundException();
+        }
+
+    }
+
+    /**
+     * Get the download url for XWiki
+     * @param $version
+     * @param bool $xem
+     * @return mixed
+     * @throws \XWiki\Exceptions\VersionNotFoundException
+     */
+    public function getXWikiVersionUrl($version, $xem = false) {
+        $sub = $xem?"xem":"xe";
+        if(isset($this->versions['version'][$sub][$version])) {
+            return $this->versions['version'][$sub][$version];
+        }
+
+        throw new VersionNotFoundException("Version {$version} not found");
+    }
 }
